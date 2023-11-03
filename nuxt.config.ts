2fac28436101598810cabc02734a81268ddb835f
ChloéG <chloe.grazia@gmail.com>
2023-11-03 17:15:58 +0100

    changed nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  ssr: false
+    devtools: {enabled: true},
+    ssr: false,
+    generate: {
+        nojekyll: true,
+        fallback: '404.html',
+    },
+    target: "static",
+    app: {
+        baseURL: "/naokiji/"
+    }
 })
