4a609e0af72dcb333fa34d1af4aa25da536ab5d2
ChloéG <chloe.grazia@gmail.com>
2023-11-16 17:27:59 +0100

    Fix button style bug

--- components/BaseKanji.vue
+++ components/BaseKanji.vue
@@ -1,35 +1,37 @@
 <template>
-  <section class="font-line bg-duo-grey-dark text-white min-h-screen">
-    <div class="container mx-auto text-center">
+  <section class="flex flex-col justify-between min-h-screen font-line bg-duo-grey-dark text-white text-center">
+    <div class="">
       <h1 class="font-bold text-3xl pt-3">Kanji Trainer</h1>
       <p class="text-xl mt-1">What's this Kanji ?</p>
       <p class="text-5xl font-bold mt-8">{{ question.kanji }}</p>
+    </div>
 
-      <div class="mt-10 grid grid-cols-2 gap-y-5 gap-x-4 px-6">
-        <ChoiceButton
-            class="font-bold"
-            v-for="answer in answerPoolReading"
-            :is-right="readingChoice === answer && readingChoice === question.reading"
-            :is-wrong="readingChoice === answer && readingChoice !== question.reading"
-            @click="readingChoice = answer"
-        >
-          {{ answer }}
-        </ChoiceButton>
-      </div>
+    <div class="grid grid-cols-2 gap-y-5 gap-x-4 px-6">
+      <ChoiceButton
+          class="font-bold"
+          v-for="answer in answerPoolReading"
+          :is-right="readingChoice === answer && readingChoice === question.reading"
+          :is-wrong="readingChoice === answer && readingChoice !== question.reading"
+          @click="readingChoice = answer"
+      >
+        {{ answer }}
+      </ChoiceButton>
+    </div>
 
-      <div class="mt-10 grid grid-cols-2 gap-y-5 gap-x-4 px-6">
-        <ChoiceButton
-            v-for="answer in answerPoolMeaning"
-            :is-right="meaningChoice === answer && meaningChoice === question.meaning"
-            :is-wrong="meaningChoice === answer && meaningChoice !== question.meaning"
-            @click="meaningChoice = answer"
-        >
-          {{ answer }}
-        </ChoiceButton>
-      </div>
+    <!--    <div class="mt-10 grid grid-cols-2 gap-y-5 gap-x-4 px-6">-->
+    <div class="grid grid-cols-2 gap-y-5 gap-x-4 px-6">
+      <ChoiceButton
+          v-for="answer in answerPoolMeaning"
+          :is-right="meaningChoice === answer && meaningChoice === question.meaning"
+          :is-wrong="meaningChoice === answer && meaningChoice !== question.meaning"
+          @click="meaningChoice = answer"
+      >
+        {{ answer }}
+      </ChoiceButton>
+    </div>
+    <div class="mb-4">
       <ActionButtonBlue
           @click="reload"
-          class="mt-12"
       >
         NEXT
       </ActionButtonBlue>
@@ -54,7 +56,7 @@ const meaningChoice = ref(undefined);
 
 function getRandomAnswer() {
   let answerPool = data.filter(function (item) {
-    return item !== question.value;
+    return item.kanji !== question.value.kanji;
   })
   answerPool = shuffle(answerPool).slice(0, 3);
   answerPool.push(question.value);
--- components/ChoiceButton.vue
+++ components/ChoiceButton.vue
@@ -1,7 +1,8 @@
 <template>
   <button
-      class="text-xl bg-duo-grey-dark ring ring-inset ring-duo-grey-lighter border-b-2 border-duo-grey-lighter rounded-xl min-h-[95px] px-2 active:bg-duo-grey-light active:border-b-0 active:border-t-2 active:border-duo-grey-dark active:ring-duo-blue-dark active:text-duo-blue-dark"
+      class="text-xl bg-duo-grey-dark ring ring-inset  border-b-2  rounded-xl min-h-[95px] px-2 active:bg-duo-grey-light active:border-b-0 active:border-t-2 active:border-duo-grey-dark active:ring-duo-blue-dark active:text-duo-blue-dark"
       :class="{
+    'ring-duo-grey-lighter border-duo-grey-lighter': !props.isRight && !props.isWrong,
       'text-duo-green ring-duo-green border-b-duo-green': props.isRight,
       'text-duo-red ring-duo-red border-b-duo-red': props.isWrong,
     }"
